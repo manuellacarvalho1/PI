26eca743f12d6bcffe7c2a8d81b8388e6a1b5018
Manuella Carvalho <manuellarodrigues4@gmail.com>
2026-06-24 00:24:15 -0300

    feat: ajuste e  salvamento de resultados corretos no CSV

--- estrutura-dados-busca-sequencial/tests/testes_busca.c
+++ estrutura-dados-busca-sequencial/tests/testes_busca.c
@@ -21,13 +21,14 @@ void executar_experimento(char* nome_arquivo) {
 
     // Medir o tempo
     double tempo_medio = calcular_tempo_sequencial(produtos, quantidade, ids, 1000);
-    double tempo_total = tempo_medio * 1000;
+    double tempo_total = tempo_medio;           // tempo total das 1000 buscas
+    double tempo_medio_por_busca = tempo_medio / 1000; // tempo médio por busca
 
     // Salvar no CSV de resultados
     FILE* f = fopen("../results/resultados_testes.csv", "a");
     if(f) {
-    fprintf(f, "sequencial -> %-18s | %-15d | %-20.10f | %-20.10f | %-10s\n", 
-        nome_arquivo, quantidade, tempo_total, tempo_medio, "-");        
+    fprintf(f, "sequencial -> %-18s | %-15d | %-20.10f | %-20.10f | %-10s\n",
+        nome_arquivo, quantidade, tempo_total, tempo_medio_por_busca, "-");      
     fclose(f);
     }
 
